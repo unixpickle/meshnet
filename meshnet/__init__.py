f47c78fac492c5c9be4bcf856c9860a03ece7309
Alex Nichol <unixpickle@gmail.com>
2018-12-29 10:43:10 -0500

    module works

--- meshnet/__init__.py
+++ meshnet/__init__.py
@@ -3,7 +3,6 @@ A neural network based on spring meshes.
 """
 
 import torch
-import torch.autograd as autograd
 import torch.nn as nn
 
 
@@ -13,7 +12,7 @@ class MeshLayer(nn.Module):
     factorization.
     """
 
-    def __init__(self, num_inputs, num_outputs, space_dims=2, init_delta=0.1):
+    def __init__(self, num_inputs, num_outputs, space_dims=2, init_delta=0.01):
         super().__init__()
         self.num_inputs = num_inputs
         self.num_outputs = num_outputs
@@ -22,18 +21,18 @@ class MeshLayer(nn.Module):
         init_in_pos = torch.randn((num_inputs, 1, space_dims))
         init_out_pos = torch.randn((1, num_outputs, space_dims))
         self.init_dists = _distance_matrix(
-            self.init_in_pos + init_delta * torch.randn((num_inputs, 1, space_dims)),
-            self.init_out_pos + init_delta * torch.randn((1, num_outputs, space_dims)),
+            init_in_pos + init_delta * torch.randn((num_inputs, 1, space_dims)),
+            init_out_pos + init_delta * torch.randn((1, num_outputs, space_dims)),
         )
-        self.in_pos = autograd.Variable(init_in_pos)
-        self.out_pos = autograd.Variable(init_out_pos)
-        self.biases = autograd.Variable(torch.zeros(num_outputs))
+        self.in_pos = nn.Parameter(init_in_pos)
+        self.out_pos = nn.Parameter(init_out_pos)
+        self.biases = nn.Parameter(torch.zeros(num_outputs))
 
     def forward(self, inputs):
         dists = _distance_matrix(self.in_pos, self.out_pos)
         weights = dists - self.init_dists
-        return torch.matmul(inputs, weights) + self.Biases
+        return torch.matmul(inputs, weights) + self.biases
 
 
 def _distance_matrix(in_pos, out_pos):
-    return torch.sqrt(torch.sum(torch.square(in_pos - out_pos), dim=-1))
+    return torch.sqrt(torch.sum((in_pos - out_pos) ** 2, dim=-1))
